79dd1af670bd6d540c2f3526d8141befa2cd3146
rzn <meksikietis222@gmail.com>
2024-06-03 03:21:05 +0300

    asd

--- app.vue
+++ app.vue
@@ -31,7 +31,7 @@ onMounted(() => {
       const analyser = new AudioAnalyser(sound, 64);
 
       camera.aspect = window.innerWidth / window.innerHeight;
-      camera.position.set(0, 0, 38);
+      camera.position.set(0, 0, 40);
       camera.updateProjectionMatrix();
 
       function animate() {
