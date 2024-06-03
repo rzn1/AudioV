0122c05327ea9395854ef82ffb7cebef84892d36
rzn <meksikietis222@gmail.com>
2024-06-03 03:58:15 +0300

    asd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@tresjs/nuxt'],
+
+  app: {
+    head: {
+      title: 'Melancholy Hues 💫',
+      meta: [
+        { name: 'description', content: '💫💫💫💫💫💫.' }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', sizes: '32x32', href: '/favicon.ico' }]
+    }
+  },
 })
